32ca0b852f379cbdfd0bb3b2f90502a8b1b5f4cd
niwanowa <40478818+niwanowa@users.noreply.github.com>
2023-11-26 18:22:51 +0900

    reset API : S3へのファイル配置実装

--- niwanowa_rssfeed_functions/reset/reset.py
+++ niwanowa_rssfeed_functions/reset/reset.py
@@ -4,7 +4,7 @@
       summary: 'RSSFeedをリセット(初回生成)するAPI'
       responses:
         '200':
-          description: 'Content added to the RSS feed'
+          description: 'Initialized RSSFeed'
       x-amazon-apigateway-integration:
         httpMethod: post
         type: aws_proxy
@@ -13,14 +13,28 @@
 """
 import json
 import boto3
+import os
 
 def lambda_handler(event, context):
     # S3クライアント作成
     s3 = boto3.client('s3')
-    bucket = 'niwanowa-rssfeed'
+    BUCKET_NAME = os.environ['BUCKET_NAME']
 
-    # S3からXMLを取得対象ファイルがない場合はエラーを出力する
-    
+    # S3のrss.xmlを固定ファイルで置換する。
+    s3.put_object(
+        Bucket=BUCKET_NAME,
+        Body="""<?xml version="1.0" encoding="UTF-8"?>
+            <rss version="2.0">
+                <channel>
+                    <title>にわのわのRSS</title>
+                    <link>http://yourwebsite.com</link>
+                    <description>Your feed description</description>
+                </channel>
+            </rss>""",
+        Key='rss.xml',
+        ContentType='application/xml'
+    )
+
     # Lambdaプロキシ統合に対応したレスポンスを返す
     response = {
         "statusCode": 200,
