1b55fffc0fc70860a220109a836a6e301f3f59c3
niwanowa <40478818+niwanowa@users.noreply.github.com>
2023-11-27 06:18:39 +0900

    post S3アクセス実装

--- niwanowa_rssfeed_functions/post/post.py
+++ niwanowa_rssfeed_functions/post/post.py
@@ -20,21 +20,38 @@ post:
 
 import json
 import boto3
+import os
+import feedgenerator
+import feedparser
+from datetime import datetime
 
 def lambda_handler(event, context):
     # S3クライアント作成
     s3 = boto3.client('s3')
-    bucket = 'niwanowa-rssfeed'
 
-    # S3からXMLを取得対象ファイルがない場合はエラーを出力する
-    
+    # 環境変数からS3のバケット名とファイル名を取得
+    BUCKET_NAME = os.environ['BUCKET_NAME']
+    FILE_NAME = os.environ['FILE_NAME']
+
+    # rssファイル読み込み
+    obj = s3.get_object(Bucket=BUCKET_NAME, Key=FILE_NAME)
+    rss_xml_string = obj['Body'].read().decode('utf-8')
+
+    # feedparserでパース
+    feed = feedparser.parse(rss_xml_string)
+
+    print(feed)
+
+    # リクエストボディからlinkを取得
+    link = event['link']
+
+    print(link)
+
     # Lambdaプロキシ統合に対応したレスポンスを返す
     response = {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps({"message": "Content added to the RSS feed"})
+      "statusCode": 200,
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "body": json.dumps({"message": "Content added to the RSS feed"})
     }
-
-    return response
