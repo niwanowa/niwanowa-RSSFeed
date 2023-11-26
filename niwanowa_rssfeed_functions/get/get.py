94f0a1424a4e5ac83a92a2e0837857eee7440d8f
niwanowa <40478818+niwanowa@users.noreply.github.com>
2023-11-26 19:32:06 +0900

    getAPIを実際のS3を指すように修正

--- niwanowa_rssfeed_functions/get/get.py
+++ niwanowa_rssfeed_functions/get/get.py
@@ -23,13 +23,23 @@ get:
 """
 import json
 import boto3
+import os
 
 def lambda_handler(event, context):
     # S3クライアント作成
     s3 = boto3.client('s3')
-    bucket = 'niwanowa-rssfeed'
+    BUCKET_NAME = os.environ['BUCKET_NAME']
 
-    # S3からXMLを取得対象ファイルがない場合はエラーを出力する
+    # S3からrss.xmlを取得。
+    # 対象ファイルがない場合はエラーを出力する
+    try:
+        response = s3.get_object(
+            Bucket=BUCKET_NAME,
+            Key='rss.xml'
+        )
+    except Exception as e:
+        print(e)
+        raise e
     
     # Lambdaプロキシ統合に対応したレスポンスを返す
     response = {
@@ -37,14 +47,23 @@ def lambda_handler(event, context):
         "headers": {
             "Content-Type": "application/xml"
         },
-        "body": """<?xml version="1.0" encoding="UTF-8"?>
-            <rss version="2.0">
-                <channel>
-                    <title>Your RSS Feed</title>
-                    <link>http://yourwebsite.com</link>
-                    <description>Your feed description</description>
-                </channel>
-            </rss>"""
+        "body": response['Body'].read().decode('utf-8')
     }
+
+
+    # response = {
+    #     "statusCode": 200,
+    #     "headers": {
+    #         "Content-Type": "application/xml"
+    #     },
+    #     "body": """<?xml version="1.0" encoding="UTF-8"?>
+    #         <rss version="2.0">
+    #             <channel>
+    #                 <title>Your RSS Feed</title>
+    #                 <link>http://yourwebsite.com</link>
+    #                 <description>Your feed description</description>
+    #             </channel>
+    #         </rss>"""
+    # }
 
     return response
