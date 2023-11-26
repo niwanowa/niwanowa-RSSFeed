c95464ca61b7dc9a7bb14ebfc2c8b6feec14d179
niwanowa <40478818+niwanowa@users.noreply.github.com>
2023-11-26 22:09:25 +0900

    reset.py実装

--- niwanowa_rssfeed_functions/reset/reset.py
+++ niwanowa_rssfeed_functions/reset/reset.py
@@ -14,24 +14,53 @@
 import json
 import boto3
 import os
+import feedgenerator
+from datetime import datetime
 
 def lambda_handler(event, context):
     # S3クライアント作成
     s3 = boto3.client('s3')
-    BUCKET_NAME = os.environ['BUCKET_NAME']
 
-    # S3のrss.xmlを固定ファイルで置換する。
+    # 環境変数からS3のバケット名とファイル名を取得
+    BUCKET_NAME = os.environ['BUCKET_NAME']
+    FILE_NAME = os.environ['FILE_NAME']
+
+    # rssファイルの要素を初期化する。
+    feed_title = "にわのわのRSS"
+    feed_link = "https://example.com/rss"
+    feed_description = "にわのわさんのRSSFeed。"
+    feed_language = "ja"
+    feed_author = "にわのわ"
+
+    # エントリーの要素を変数で代入（例として2つのエントリーを生成）
+    entry_title = "にわのわさんのブログ"
+    entry_link = "https://hugo.niwanowa.tips/"
+    entry_summary = "にわのわさんがやってるブログだよ。"
+
+    # RSSフィードの基本構造を生成
+    feed = feedgenerator.Atom1Feed(
+        title=feed_title,
+        link=feed_link,
+        description=feed_description,
+        language=feed_language,
+        author_name=feed_author,
+    )
+
+    # フィードにエントリーを追加
+    feed.add_item(
+        title=entry_title,
+        link=entry_link,
+        description=entry_summary,
+    )
+
+    # RSSフィードのXMLを生成
+    rss_xml_string = feed.writeString('utf-8')
+
+    # S3のrss.xmlを置換する。
     s3.put_object(
         Bucket=BUCKET_NAME,
-        Body="""<?xml version="1.0" encoding="UTF-8"?>
-            <rss version="2.0">
-                <channel>
-                    <title>にわのわのRSS</title>
-                    <link>http://yourwebsite.com</link>
-                    <description>Your feed description</description>
-                </channel>
-            </rss>""",
-        Key='rss.xml',
+        Body=rss_xml_string.encode('utf-8'),
+        Key=FILE_NAME,
         ContentType='application/xml'
     )
 
