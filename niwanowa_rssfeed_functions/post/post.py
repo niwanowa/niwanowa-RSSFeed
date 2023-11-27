caeb7d79646fd22d2a84ba6c5ac5073c86c1ea89
niwanowa <40478818+niwanowa@users.noreply.github.com>
2023-11-27 16:25:28 +0900

    post処理実装

--- niwanowa_rssfeed_functions/post/post.py
+++ niwanowa_rssfeed_functions/post/post.py
@@ -24,8 +24,12 @@ import os
 import feedgenerator
 import feedparser
 from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+import time
 
 def lambda_handler(event, context):
+    print(event)
     # S3クライアント作成
     s3 = boto3.client('s3')
 
@@ -38,14 +42,62 @@ def lambda_handler(event, context):
     rss_xml_string = obj['Body'].read().decode('utf-8')
 
     # feedparserでパース
-    feed = feedparser.parse(rss_xml_string)
+    feed_parser = feedparser.parse(rss_xml_string)
 
-    print(feed)
+    print(feed_parser)
+
+    # FeedParserDictからfeedgeneratorのAtom用Feedオブジェクトを作成
+    feed_generator = feedgenerator.Atom1Feed(
+        title=feed_parser.feed.title,
+        link=feed_parser.feed.link,
+        description=feed_parser.feed.subtitle,
+        language=feed_parser.feed.language,
+        author_name=feed_parser.feed.author,
+    )
+
+
+
+    # Feedオブジェクトにパーサーのエントリーを追加
+    for entry in feed_parser.entries:
+        # print(entry.updated_parsed)
+        # print(type(entry.updated_parsed))
+        # print(datetime.fromtimestamp(time.mktime(entry.updated_parsed)))
+        # print(type(datetime.fromtimestamp(time.mktime(entry.updated_parsed))))
+        feed_generator.add_item(
+            title=entry.title,
+            link=entry.link,
+            description=entry.summary,
+            pubdate=datetime.fromtimestamp(time.mktime(entry.updated_parsed)),
+        )
+
+
+    print(feed_generator.writeString('utf-8'))
 
     # リクエストボディからlinkを取得
-    link = event['link']
+    link = json.loads(event['body']).get('link')
 
-    print(link)
+    # linkからページタイトルを取得
+    page_title = get_page_title(link)
+
+    # feedにエントリー追加
+    feed_generator.add_item(
+        title=page_title,
+        link=link,
+        description=page_title,
+        pubdate=datetime.now(),
+    )
+
+    # Stringに書き出し
+    rss_xml_string = feed_generator.writeString('utf-8')
+
+    # #s3にアップロード
+    s3.put_object(
+        Bucket=BUCKET_NAME,
+        Body=rss_xml_string.encode('utf-8'),
+        Key=FILE_NAME,
+        ContentType='application/xml'
+    )
+
 
     # Lambdaプロキシ統合に対応したレスポンスを返す
     response = {
@@ -57,3 +109,16 @@ def lambda_handler(event, context):
     }
 
     return response
+
+def get_page_title(link):
+    # ページタイトルを取得する。
+    # ページタイトルが取得できない場合は、空文字を返す。
+
+    try:
+        response = requests.get(link)
+        soup = BeautifulSoup(response.text, "html.parser")
+        page_title = soup.title.string if soup.title else None
+        return page_title
+    except Exception as e:
+      print(e)
+      return None
